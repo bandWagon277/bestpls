8d031beb550d9bf2da84b5c3fc9c25c280f86c36
bandWagon277 <152815469+bandWagon277@users.noreply.github.com>
2023-12-13 21:38:42 -0500

    Update simpSPLS.R

--- R/simpSPLS.R
+++ R/simpSPLS.R
@@ -8,6 +8,8 @@
 #' Y=matrix(rnorm(100),5,20)
 #' fit = sparsePLS(X,Y,2)
 #' @importFrom pls plsr
+#' @import Rcpp
+#' @import RcppArmadillo
 #' @export
 
 #sourceCpp("src/splsCpp.cpp")
@@ -130,3 +132,47 @@ Weight_vec_cal <-function( Z, eta, kappa, eps, maxstep ){
   }
   return(c)
 }
+
+cppFunction('arma::vec Ust(arma::vec b, double eta) {
+   int n = b.n_elem;
+   arma::vec b_ust(n);
+   arma::vec sign_b = sign(b);
+   if (eta < 1) {
+     arma::vec valb = abs(b) - eta * max(abs(b));
+     for (int i = 0; i < n; i++) {
+       if (valb(i) >= 0) {
+         b_ust(i) = valb(i) * sign_b(i);
+       }
+     }
+   }
+   return b_ust;
+ }',depends = "RcppArmadillo")
+
+cppFunction('arma::mat case_1(arma::mat M,arma::vec c, double eps,int maxstep,double eta){
+  double dis = 10;
+  int i = 1;
+  arma::vec c_new = c;
+
+  while (dis > eps && i <= maxstep) {
+    // Optimize a for fixed c
+    arma::mat U;
+    arma::mat V;
+    arma::vec s;
+    svd(U, s, V, M * c);
+    arma::mat a = U.col(0) * V.t();
+
+
+    // Soft thresholding for optimizing c (assuming lambda2 -> Inf)
+    arma::mat ma = M*a;
+    arma::vec ma_v = ma.col(0);
+    c_new = Ust(ma_v, eta);
+
+    // Calculate discrepancy between a & c
+    dis = max(abs(c-c_new));
+    c = c_new;
+    i++;
+  }
+  return c;
+}',depends = "RcppArmadillo")
+
+
